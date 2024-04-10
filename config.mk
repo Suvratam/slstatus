78e8232c10ad4e263890d1ac6b551fd020fa50e8
suvaratam <suvaratampatra@gmail.com>
2024-04-10 23:18:46 +0530

    Add config

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # slstatus version
-VERSION = 1.0
+VERSION = 0
 
 # customize below to fit your system
 
@@ -11,11 +11,9 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # flags
-CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
+CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
+CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
 LDFLAGS  = -L$(X11LIB) -s
-# OpenBSD: add -lsndio
-# FreeBSD: add -lkvm -lsndio
 LDLIBS   = -lX11
 
 # compiler and linker
